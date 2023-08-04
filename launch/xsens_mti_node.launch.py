5245987d026ff9b2a76247ec528ddcdb971b94ae
VladimirL <vleushkin@gmail.com>
2023-08-04 11:50:05 +0300

    deprecated env variable RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED fix

--- launch/xsens_mti_node.launch.py
+++ launch/xsens_mti_node.launch.py
@@ -9,8 +9,8 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     # Set env var to print messages to stdout immediately
-    arg = SetEnvironmentVariable('RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1')
-    ld.add_action(arg)
+    ld.add_action(SetEnvironmentVariable('RCUTILS_LOGGING_USE_STDOUT', '1'))
+    ld.add_action(SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'))
 
     parameters_file_path = Path(get_package_share_directory('bluespace_ai_xsens_mti_driver'), 'param', 'xsens_mti_node.yaml')
     xsens_mti_node = Node(
